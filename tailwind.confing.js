d503672f8fbca3d3bca6e14775574c491aee2d89
Amirabbas Eshghi <amirabbas.eshghi1@gmail.com (youremail@example.com)>
2025-04-25 14:51:21 +0330

    first commit

---
+++ tailwind.confing.js
@@ -0,0 +1,8 @@
+module.exports = {
+    darkMode: 'class', // باید این خط باشه
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // مطمئن شو که مسیر فایل‌ها درست باشه
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }  
